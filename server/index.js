514d8316bfc0c4cc70f6c92297fa0a4a0c648c9d
Devesh Kandpal <kandpal.d@husky.neu.edu>
2018-11-29 07:35:30 -0500

    added full stack app

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./model/User');
+require('./services/passport');
+const keys = require('./config/keys');
+
+
 const app = express();
+app.use(cookieSession({
+    maxAge : 30 * 24 * 60 * 60 * 1000,
+    keys : [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-app.get('/', (req,res) => {
+require('./routes/authRoutes')(app);
 
-});
-
+mongoose.connect(keys.mongoUri);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
