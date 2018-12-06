7f25c5a9ebc6bf93b2b05f18bce32f07dfcadc60
Devesh Kandpal <kandpal.d@husky.neu.edu>
2018-12-06 06:58:55 -0500

    refactored client code and made changes for proxy middleware

---
+++ server/client/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            Hi There!
+            <a href="/auth/google">Sign in with google</a>
+        </div>
+    );
+};
+
+export default App;
